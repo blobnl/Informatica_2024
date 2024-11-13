08b811a6ffb4bcec672c313c6e1bdf290449f775
blobnl <andrea.bottino@polito.it>
2024-11-13 11:36:05 +0100

    w08

--- W07/tris.py
+++ W07/tris.py
@@ -27,7 +27,42 @@ def inserisciMossa(griglia, player):
     pass
 
 def controllaGriglia(griglia):
-    pass
+    """
+    :param griglia: la griglia di gioco
+    :return: True se la partita è terminata, False altrimenti
+    """
+    # orizzontali
+    for riga in griglia:
+        if riga[0] != " " and riga[0] == riga[1] and riga[1] == riga[2]:
+            stampaVincitore(riga[0])
+            return True
+
+    # verticali
+    for colonna in range(len(riga[0])):
+        if griglia[0][colonna] != " " and griglia[0][colonna] == griglia[1][colonna] \
+            and griglia[1][colonna] == griglia[2][colonna]:
+            stampaVincitore(griglia[0][colonna])
+            return True
+
+    # diagonali
+    if griglia[0][0] != " " and griglia[0][0] == griglia[1][1] \
+        and griglia[1][1] == griglia[2][2]:
+        stampaVincitore(griglia[0][0])
+        return True
+
+    if griglia[0][2] != " " and griglia[0][2] == griglia[1][1] \
+        and griglia[1][1] == griglia[2][0]:
+        stampaVincitore(griglia[0][2])
+        return True
+
+    # controlla pareggio --> se c'è almeno una casella libera, niente pareggio, altrimenti pareggio
+    for riga in griglia:
+        if " " in riga:
+            return False
+
+
+    print("Non ci sono più mosse disponibili. Pareggio")
+    return True
 
 def main():
     PLAYER_1 = 'X'
---
+++ W08/ca2.py
@@ -0,0 +1,20 @@
+'''
+scrivere un programma che sia ingrado di fare la conversione in ca2
+su 8 bit di un numero letto da tastiera
+
+'''
+
+BITS = 8
+
+def num2bin(numero):
+    pass
+
+def ca2(numero):
+    pass
+
+def main():
+    pass
+
+main()
+            
+    
